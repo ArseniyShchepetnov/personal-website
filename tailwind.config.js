2b3320e8c911cc497c24d9d934864a8c61533ae5
Arseniy Shchepetnov <ash@inovintell.com>
2023-06-04 00:30:09 +0400

    Commit main files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        ...colors
+      }
+    },
+  },
+  plugins: [],
+}
